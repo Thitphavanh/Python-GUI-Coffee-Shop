2e0faa2211e8f144e4eeae06ef6f60c459e72565
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-22 08:54:30 +0700

    update

--- productdb.py
+++ productdb.py
@@ -10,6 +10,41 @@ c.execute("""CREATE TABLE IF NOT EXISTS product (
                 price REAL,
                 image TEXT)""")
 
+c.execute("""CREATE TABLE IF NOT EXISTS product_status (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                product_id INTEGER,
+                status TEXT)""")
+
+
+def InsertProductStatus(product_id, status):
+    check = ViewProductStatus(product_id)
+    if check == None:
+        with conn:
+            command = 'INSERT INTO product_status VALUES (?,?,?)'
+            c.execute(command, (None, product_id, status))
+        conn.commit()
+        print('status saved')
+    else:
+        print('product_id exist !')
+        print(check)
+        UpdateProductStatus(product_id, status)
+
+
+def ViewProductStatus(product_id):
+    # READ
+    with conn:
+        command = 'SELECT * FROM product_status WHERE product_id = (?)'
+        c.execute(command, ([product_id]))
+        result = c.fetchone()
+    return result
+
+def UpdateProductStatus(product_id, status):
+    # UPDATE
+    with conn:
+        command = 'UPDATE product_status SET status = (?) WHERE ID = (?)'
+        c.execute(command, ([status, product_id]))
+    conn.commit()
+    print('updated :' ,(product_id,status))
 
 def InsertProduct(productid, title, price, image):
     # CREATE
@@ -53,4 +88,6 @@ def ViewProductSingle(productid):
 if __name__ == '__main__':
     # InsertProduct('Coffee-1002', 'Americano', '17000', r'C:\Image\americano.png')
     # ViewProduct()
-    ViewProductTableIcon()
+    # ViewProductTableIcon()
+    InsertProductStatus(1,'show')
+    # print(ViewProductStatus(1))
